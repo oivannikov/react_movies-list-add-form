9c0cf6603815c3e85ebfb3620ea3514805930841
oivannikov <alex0502ivannikov@gmail.com>
2020-09-09 21:32:45 +0300

    Updates

---
+++ precache-manifest.e4b1674580acb7c2bde4d18137882374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f10ecddeabfc37618ba58a33cf7a0a24",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51e092dbfe9e1e1dc05d",
+    "url": "./static/css/2.67a0cd86.chunk.css"
+  },
+  {
+    "revision": "a91a79877adcc7ca4e8d",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "51e092dbfe9e1e1dc05d",
+    "url": "./static/js/2.bf80cdcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bf80cdcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91a79877adcc7ca4e8d",
+    "url": "./static/js/main.dcd49705.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
